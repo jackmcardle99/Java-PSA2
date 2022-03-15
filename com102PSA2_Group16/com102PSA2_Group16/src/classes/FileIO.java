5504f7121e2b95eb5384ea48b1a3764869a95164
Jack McArdle <66914827+jackmcardle99@users.noreply.github.com>
2022-03-15 13:50:59 +0000

    Librarian can now issue new loan, writes to csv on termination, minor changes to methods in loan class

---
+++ com102PSA2_Group16/com102PSA2_Group16/src/classes/FileIO.java
@@ -0,0 +1,164 @@
+package classes;
+import java.io.*;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FileIO 
+{
+    //initialize arraylists for csv files
+    private static ArrayList<Items> itemList = new ArrayList<Items>();
+    private static ArrayList<Users> userList = new ArrayList<Users>();
+    private static ArrayList<Loans> loanList = new ArrayList<Loans>();
+    
+    //declaring class variables   
+    private Scanner scan;
+    private BufferedReader inputStream;
+    private BufferedWriter outputStream;
+    private FileReader fr;
+    private FileWriter fw;
+    private String fileIn, fileOut, line;    
+    
+    //getters for arraylists, so we can use in other classes if needed
+    public ArrayList<Items> getItemList()
+    {
+        return itemList;
+    }
+    
+    public ArrayList<Users> getUserList()
+    {
+        return userList;
+    }
+    
+    public ArrayList<Loans> getLoanList()
+    {
+        return loanList;
+    }
+    
+    //method for reading ITEMS.csv
+    public void readItems() throws IOException 
+    {   
+    fileIn = "ITEMS.csv";
+    fileOut = ""; //when writing to file
+    line = null;
+       
+    // Read all lines in from CSV file and add to itemList
+        FileReader fileReader = new FileReader(fileIn);
+        inputStream = new BufferedReader(fileReader);
+        inputStream.readLine();
+
+        while ((line = inputStream.readLine()) != null) {
+            String[] temp = line.split(",");
+            String barcode = temp[0];
+            String author = temp[1];
+            String title = temp[2];
+            String type = temp[3];
+            short year = Short.parseShort(temp[4]);
+            String isbn = temp[5];
+            itemList.add(new Items(barcode, author, title, type, year, isbn));
+        }
+        inputStream.close();             
+    }
+    
+    //Method for reading USERS.csv
+    public void readUsers() throws IOException
+    {
+     fileIn = "USERS.csv";
+     fileOut = ""; //when writing to file
+     line = null;  
+        
+    // Read all lines in from CSV file and add to itemList
+        fr = new FileReader(fileIn);
+        inputStream = new BufferedReader(fr);
+        inputStream.readLine();
+
+        while ((line = inputStream.readLine()) != null) { 
+            String[] temp = line.split(",");
+            String userID = temp[0];
+            String forename = temp[1];
+            String surname = temp[2];
+            String email = temp[3];
+
+            userList.add(new Users(userID, forename, surname, email));
+        }
+        inputStream.close();             
+    }       
+    
+    //method for reading LOANS.csv
+    public void readLoans() throws IOException, Exception 
+    {
+    fileIn = "LOANS.csv";
+    fileOut = ""; //when writing to file
+    line = null; 
+        
+    // Read all lines in from CSV file and add to itemList
+        fr = new FileReader(fileIn);
+        inputStream = new BufferedReader(fr);
+        inputStream.readLine();
+
+        while ((line = inputStream.readLine()) != null) {
+            String[] temp = line.split(",");
+            String barcode = temp[0];
+            String userID = temp[1];
+            String issueDate = temp[2];
+            String dueDate = temp[3];
+            int numRenews = Integer.parseInt(temp[4]);
+            loanList.add(new Loans(barcode, userID, issueDate, dueDate, numRenews));
+        }
+        inputStream.close();    
+    }  
+    
+    //methods for printing list ArrayList summaries
+    public void printItemSummary()
+    {
+        for(Items items: itemList) {  
+            System.out.println(items);        //loops through arrays and prints line by line
+        }
+    }
+    
+    public void printUserSummary()
+    {
+        for(Users users : userList)
+        {
+            System.out.println(users);
+        }
+    } 
+    
+    public void printLoanSummary()
+    {         
+        for(Loans loans : loanList)
+        {
+            System.out.println(loans);
+        }
+    }
+    
+    //this method is temporary and pointless, code is here so i know what to do later on
+    public void printUser()
+    {
+        Users user = userList.get(1);
+        System.out.println(user.getUserID());
+    }
+
+    public void writeToLoans(String newLoan) // WHEN PROGRAM EXITS THIS IS CALLED
+    {      
+        try {
+            fileOut = "LOANS.csv";
+            fw = new FileWriter(fileOut, true);
+            outputStream = new BufferedWriter(fw);
+            outputStream.write(newLoan + "\n");             
+            outputStream.close();
+        } catch (IOException ex) {
+            Logger.getLogger(FileIO.class.getName()).log(Level.SEVERE, null, ex);
+        }    
+    }
+    
+    public void clearArray()
+    {
+       itemList.clear();
+       loanList.clear();
+       userList.clear();
+       
+    }
+    
+}
+
---
+++ com102PSA2_Group16/com102PSA2_Group16/src/classes/Loans.java
@@ -0,0 +1,189 @@
+package classes;
+import java.io.IOException;
+import java.util.*;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
+public class Loans {
+    
+// declaring class variables
+    private String bardcode, userID, issueDate, dueDate, loanType;
+    private LocalDate date;
+    private DateTimeFormatter dateFormat;
+//declaring as final means we cannot alter values 
+    private final int mediaLoan =7, bookLoan = 28, maxBookRenew = 3, maxMediaRenew = 2;     
+    private int renewCount; //counting number of renewals 
+    private Scanner scan;
+    FileIO file = new FileIO();
+    Items item1 = new Items();
+    private String inputUserId, inputBarcode;
+//creating instances of arraylists from fileio class
+    ArrayList<Users> userList = file.getUserList();
+    ArrayList<Items> itemList = file.getItemList(); 
+    ArrayList<Loans> loanList = file.getLoanList();
+    
+    public Loans(String barcode, String userID, String issueDate, String dueDate, int renewCount)
+    {
+        this.bardcode = barcode;
+        this.userID = userID;
+        this.issueDate = issueDate;
+        this.dueDate = dueDate;
+        this.renewCount = renewCount;       
+    }
+    
+    //overload contructor! Dont remove
+    public Loans()
+    {   
+    }
+ 
+    public void loanEligibility() throws IOException
+    {
+        String loanType = null;
+        boolean available = false, loanBarcodeFound = false, itemBarcodeFound = false, userFound = false;
+        scan = new Scanner(System.in);
+ 
+        //getting user input for new loan details
+        System.out.println("\nPlease enter User ID for loan. ");
+        inputUserId = scan.nextLine();
+        System.out.println("Please enter barcode for loan. ");
+        inputBarcode = scan.nextLine();
+        
+        //looping through users to ensure that they exist
+        for (Users user : userList)
+        {
+            if (inputUserId.equals(user.getUserID()))
+                userFound = true;          
+        }
+        
+//        //Looping through iteem array to ensure barcode actually exists   
+        
+      
+        for (Items item : itemList)
+        {
+            if (inputBarcode.equals(item.getBarcode()))  
+            {  
+                itemBarcodeFound = true;     
+            }                  
+        }
+ 
+        //Looping through loan array to ensure that barcode is not already in use 
+        for (Loans loan : loanList)
+        {
+            if (inputBarcode.equals(loan.getBarcode()))               
+                loanBarcodeFound = true;  
+        } 
+        
+        //decision structure to ensure loan is available
+        if (loanBarcodeFound == false && itemBarcodeFound == true)        
+                available = true;                   
+                if (available == true)
+                {
+                    System.out.println("\nThis item is available for loan!\n");                   
+                }
+                else 
+                {
+                    System.out.println("\nThis loan couldn't be created for the specified reason(s)\n");
+                    if (itemBarcodeFound == false)
+                        System.out.println(" - The item barcode doesn't exist. ");
+                    if (loanBarcodeFound == true)
+                        System.out.println(" - The item is already on loan. ");
+                    if (userFound == false)
+                        System.out.println(" - The user ID doesnt exist. ");     
+                    this.loanEligibility();
+                }       
+        this.getLoanType(inputBarcode);
+    }
+    
+    public String getLoanType(String inputBarcode) throws IOException
+    {        
+        
+        boolean found = false;
+  
+        int length = itemList.size();
+       
+ 
+        for (int i = 0; i < length; i++)
+        {
+            if (itemList.get(i).getBarcode().equals(this.inputBarcode) && itemList.get(i).getType().equals("Book"))
+            {
+                loanType = "Book";
+                
+            }
+            else if (itemList.get(i).getBarcode().equals(this.inputBarcode) && itemList.get(i).getType().equals("Multimedia"))
+            {
+                loanType = "Multimedia";         
+            } 
+        }          
+        return loanType;
+    }
+    
+     public void createLoan(String loanType) throws IOException
+    {
+        date = LocalDate.now(); //get current date in 00-00-0000 format
+        dateFormat = DateTimeFormatter.ofPattern("d/MM/yyyy"); //declare the format of 00/00/0000
+        LocalDate bookDays = date.plusDays(bookLoan); //used to add the amount of days for item type book
+        LocalDate multimediaDays = date.plusDays(mediaLoan); //used to add the amount of days for item type book
+        String issueDate = date.format(dateFormat); //get the issue date (current day)
+        String dueBook = bookDays.format(dateFormat); //get the due date for books
+        String dueMultimedia = multimediaDays.format(dateFormat); //get due date for multimedia
+        
+        String newLoan = inputBarcode + "," + inputUserId + ",";
+
+        if(loanType.equals("Book"))
+        {
+            newLoan += issueDate + "," + dueBook + "," + "0";
+        }
+        else if (loanType.equals("Multimedia"))
+        {
+            newLoan += issueDate + "," + dueMultimedia + "," + "0";
+        }
+        
+     
+        file.writeToLoans(newLoan);
+    }
+            
+    public void renewLoan()
+    {
+        //if item < 3 renewals, then renew
+        //if renew = true renewCount +=1
+        //book has max renewal of 3, media has max renewal of 2, they are declared at top of class
+    }
+    
+    public String toString()
+    {
+        String loanOutput = "Barcode: " + this.bardcode + ", " + "User ID: " + this.userID 
+                            + ", " + "Issue Date: " + this.issueDate + ", " + "Due Date: "
+                            + this.dueDate + "' " + "Renewals: " + this.renewCount;
+        return loanOutput;
+    }
+ 
+    //getters
+    public String getBarcode()
+    {
+        return this.bardcode;
+    }
+    
+    public String getUserID()
+    {
+        return this.userID;
+    }
+    
+    public String getIssueDate()
+    {
+        return this.issueDate;
+    }
+    public String getDueDate()
+    {
+        return this.dueDate;
+    }
+    
+    public int getRenewCount()
+    {
+        return this.renewCount;
+    }
+    
+    public String getLoanType()
+    {
+        return this.loanType;
+    }
+}
